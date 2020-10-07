d6ca3f009db13be4dd75f7f650430386e1d07d0c
ashandileo <ashandileonadi2001@gmail.com>
2020-10-08 03:41:07 +0700

    basic routing

--- servers.js
+++ servers.js
@@ -5,15 +5,27 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html')
 
+  let path = './views/'
+  switch(req.url) {
+    case '/': 
+      path += 'index.html'
+      break
+    case '/about':
+      path += 'about.html'
+      break
+    default:
+      path += '404.html'
+      break
+  }
+
   // send an html file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log("err", err)
-      return res.end()
+      res.end()
+    } else {
+      res.end(data)
     }
-    
-    // res.write(data)
-    res.end(data)
   })
 })
 
