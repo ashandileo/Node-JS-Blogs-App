ec91a1ed4f2f01d0b3c2a37172838145c9b41f9b
ashandileo <ashandileonadi2001@gmail.com>
2020-10-11 10:38:49 +0700

    redirect page and 404 page with express

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ app.get('/', (req, res) => {
 app.get('/about', (reqq, res) => {
   res.sendFile('./views/about.html', { root: __dirname })
 })
+
+// redirect
+app.get('/about-us', (req, res) => {
+  res.redirect('/about')
+})
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
