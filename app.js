70586e08c344416ab788937d3b89a06ea3903952
ashandileo <ashandileonadi2001@gmail.com>
2020-10-11 12:59:45 +0700

    mapping data & created parials template

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ app.set('view engine', 'ejs')
 app.listen(3000)
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const blogs = [
+      { title: 'lorem 1', snippet: 'lorem lorem lorem lorem' },
+      { title: 'lorem 2', snippet: 'lorem lorem lorem lorem' },
+      { title: 'lorem 3', snippet: 'lorem lorem lorem lorem' }
+  ]
+
+  res.render('index', { title: 'Home', blogs })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about', { title: 'About' })
 })
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create')
+  res.render('create', { title: 'Create New Blog' })
 })
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404')
+  res.status(404).render('404', { title: '404' })
 })
