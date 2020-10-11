a6427ae1777b640ca8a80d2ed1195b9769f459fc
ashandileo <ashandileonadi2001@gmail.com>
2020-10-11 11:17:48 +0700

    render ejs file

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const app = express()
 
+// register view engine
+app.set('view engine', 'ejs')
+
 app.listen(3000)
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname })
+  res.render('index')
 })
 
-app.get('/about', (reqq, res) => {
-  res.sendFile('./views/about.html', { root: __dirname })
+app.get('/about', (req, res) => {
+  res.render('about')
 })
 
-// redirect
-app.get('/about-us', (req, res) => {
-  res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+  res.render('create')
 })
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname })
+  res.status(404).render('404')
 })
