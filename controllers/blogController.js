d3687fdb5d8bbf83ea580adf0e117e636db4bc58
ashandileo <ashandileonadi2001@gmail.com>
2020-10-15 22:55:37 +0700

    controllers on nodejs

---
+++ controllers/blogController.js
@@ -0,0 +1,60 @@
+const Blog = require('../models/blog')
+
+const index = (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('blogs/index', { title: 'All Blogs', blogs: result })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const show = (req, res) => {
+  const id = req.params.id
+
+  Blog.findById(id)
+    .then(result => {
+      res.render('blogs/details', { blog: result, title: 'Blog Details' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const create = (req, res) => {
+  res.render('blogs/create', { title: 'Create New Blog' })
+}
+
+const store = (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+    .then(result => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const remove = (req, res) => {
+  const id = req.params.id
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+module.exports = {
+  index,
+  show,
+  create,
+  store,
+  remove
+}
